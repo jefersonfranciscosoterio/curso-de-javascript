a20af84d93786737a7bb6b5c1aebd192e7424e05
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-07-27 10:19:39 -0300

    finalizei

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,17 +3,20 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var dia = ('Bom Dia')
+    var tarde = ('Boa Tarde')
+    var noite = ('Boa Noite')
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12){
-        //BOM DIA!
+         msg.innerHTML = `${dia}! Agora são ${hora} horas.`
         img.src = 'manhã.png'
         document.body.style.background = '#e2cd9f'
     }else if (hora >= 12 && hora <= 18){
-        //BOA TARDE!
+        msg.innerHTML = `${tarde}! Agora são ${hora} horas.`
         img.src = 'tarde.png'
         document.body.style.background = '#b9846f'
     }else {
-        //BOA NOITE!
+        msg.innerHTML = `${noite}! Agora são ${hora} horas.` 
         img.src = 'noite.png'
         document.body.style.background = '#515154'
     }
