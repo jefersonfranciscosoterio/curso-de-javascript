7cbb3966506424abf1d8f8c88eb1a9baae913872
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-07-27 17:57:11 -0300

    falta pouco

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,45 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if(fano.value.length == 0 || fano > ano){
+    window.alert('[ERRO] verifique os dados e tente novamente!')
+   }else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked){
+        gênero = 'Homem'
+        if(idade >=0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'homem-crianca.png')
+        }else if(idade < 60){
+            //jovem
+            img.setAttribute('src', 'homem-jovem.png')
+        }else if(idade > 60){
+            //idoso
+            img.setAttribute('src', 'homem-idoso.png')
+
+    }else if (fsex[1].checked){
+        gênero = 'Mulher'
+        if(idade >=0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'mulhercrianca.png')
+        }else if(idade < 60){
+            //jovem
+            img.setAttribute('src', 'mulher-jovem.png')
+        }else if(idade > 60){
+            //idosa
+            img.setAttribute('src', 'mulher-idosa.png')
+        }
+    }
+    
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
+}   
+}
+}
