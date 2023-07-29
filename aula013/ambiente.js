2f1a027bab936fe2be113aee65af797c54be3317
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-07-29 09:49:44 -0300

    repetição com while

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,1 +1,21 @@
+//repetição com while
+
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+
+
+
+
+/*
 console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
