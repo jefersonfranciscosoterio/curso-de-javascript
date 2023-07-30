e77e8d23d47981096fe03368c4f95713395a23c2
Jeferson Dev <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-07-30 19:44:39 -0300

    Update ambiente.js

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,1 +1,25 @@
+<<<<<<< HEAD
 console.log("tudo bem?");
+=======
+//repetição com while
+
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+>>>>>>> 20c5ba6148eceaf56d4f9549c5fc52dfed7b655e
