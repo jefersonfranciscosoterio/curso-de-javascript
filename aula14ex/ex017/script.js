5746bd4884deb1a0c0748ae1e404cd257bc8eb89
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-08-01 19:01:42 -0300

    finalizei a tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ``
+        while (c <= 10) {
+            let iten = document.createElement('option')
+            iten.text = `${n} x ${c} = ${n * c}`
+            iten.value = `tab${c}`
+            tab.appendChild(iten)
+            c++
+        }
+    }
+}
