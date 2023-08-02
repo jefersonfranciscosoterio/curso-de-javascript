4f0995f4d093e43c96ca89012702d49aa3164af0
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-08-02 20:15:15 -0300

    encerrei funções

---
+++ aula016/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+}
+
+
+let res = parimpar(4)
+console.log(res)
+
+//console.log(parimpar(4))
