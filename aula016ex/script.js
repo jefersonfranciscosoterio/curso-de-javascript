5c88396dabdd4f4bde3ca4e431e7a8d0f03e9fbb
jeferson francisco soterio dos santos <133526589+jefersonfranciscosoterio@users.noreply.github.com>
2023-08-03 10:51:52 -0300

    início do script

--- aula016ex/script.js
+++ aula016ex/script.js
@@ -1,1 +1,28 @@
-alert('olá')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, 1) {
+    if (l.indexof(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
